1b217c9556952f4d2535f6a05a532be1ae77110c
Gian F Maciel <gian.f.maciel@gmail.com>
2024-10-21 22:06:23 -0300

    aula laços

---
+++ exercicio6.js
@@ -0,0 +1,21 @@
+const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}
+
+
+
+/* Qual seria o resultado impresso no console, se o usuário digitasse o número 4 ?
+
+*
+**
+***
+****
+
+*/
---
+++ exercicio8.js
@@ -0,0 +1,24 @@
+let original = [10, 5, 60, 34, 55, 81, 9, 70, 22, 48]
+
+let imprimirArray = (imprime) => {
+    
+
+}
+
+
+
+
+
+/*
+ Considere que você tenha acesso a um `array`  (chamado de 'array original') que é composto somente de números.
+ Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
+    
+    a) Escreva um programa que **imprime** cada um dos valores do array original.
+    
+    b) Escreva um programa que **imprime** cada um dos valores do array original divididos por 10
+    
+    c) Escreva um programa que **crie** um novo array contendo, somente, os números pares do array original e **imprima** esse novo array
+    
+    d) Escreva um programa que **crie** um novo array contendo strings, da seguinte forma: "O elemento do índex `i` é: `numero`". Depois, **imprima** este novo array.
+    
+    e) Escreva um programa que imprima no console o maior e o menor números contidos no array original */
